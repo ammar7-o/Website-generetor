4220d41346ba7b3d6f3a98a5bbdab1c989d194c2
ammar <166413380+ammar7-o@users.noreply.github.com>
2025-07-15 15:40:58 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,19 @@
-// Make editor global
+// // Make editor global
+// window.editor = grapesjs.init({
+//   container: '#editor',
+//   fromElement: true,
+//   height: '100vh',
+//   width: 'auto',
+//   storageManager: false,
+//   plugins: ['gjs-preset-webpage'],
+//   pluginsOpts: {
+//     'gjs-preset-webpage': {}
+//   }
+// });
+// this for add blocks from preset-webpage
+
+
+
 window.editor = grapesjs.init({
   container: '#editor',
   fromElement: true,
@@ -7,6 +22,15 @@ window.editor = grapesjs.init({
   storageManager: false,
   plugins: ['gjs-preset-webpage'],
   pluginsOpts: {
-    'gjs-preset-webpage': {}
+    'gjs-preset-webpage': {
+      blocks: [],             // disable its own basic blocks
+      blocksBasicOpts: false, // disable grapesjs-blocks-basic plugin
+      navbarOpts: false,      // prevent loading navbar plugin
+      countdownOpts: false,   // if present
+      formsOpts: false,       // disable forms plugin
+      exportOpts: false,      // disable export plugin
+      aviaryOpts: false,
+      filestackOpts: false
+    }
   }
 });
